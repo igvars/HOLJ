e7d4c17a2479f522dce1d50abb8a0a6eb12aa4b0
sergey <sergeyusatyuk@gmail.com>
2016-08-19 16:29:26 +0300

    integrate

--- views/site/error.php
+++ views/site/error.php
@@ -9,19 +9,24 @@ use yii\helpers\Html;
 
 $this->title = Yii::t('app', 'Error') . ' | ' . Yii::$app->name;
 ?>
-<div class="site-error">
+<div class="profile-page">
+    <div class="wrapper">
+        <div class="header header-filter" style="background-image: url('/images/bg.jpg');"></div>
 
-    <h1><?= Html::encode($this->title) ?></h1>
+        <div class="main main-raised">
+            <div class="profile-content">
+                <div class="container main-padding-top">
 
-    <div class="alert alert-danger">
-        <?= nl2br(Html::encode($message)) ?>
-    </div>
+                    <div class="alert alert-danger">
+                        <?= nl2br(Html::encode($message)) ?>
+                    </div>
 
-    <p>
-        The above error occurred while the Web server was processing your request.
-    </p>
-    <p>
-        Please contact us if you think this is a server error. Thank you.
-    </p>
-
-</div>
+                    <p>
+                        The above error occurred while the Web server was processing your request.
+                    </p>
+                    <p>
+                        Please contact us if you think this is a server error. Thank you.
+                    </p>
+                </div>
+            </div>
+        </div>
