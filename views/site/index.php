a4359c0c78faa727bb218951867c5b010eef4f89
sergey <sergeyusatyuk@gmail.com>
2016-08-20 00:52:38 +0300

    integrate

--- views/site/index.php
+++ views/site/index.php
@@ -91,7 +91,7 @@ $this->title = Yii::$app->name;
                             <div class="name">
                                 <h3 class="title main-padding-top"><?= Yii::t('app', 'Lesya Usatyuk')?></h3>
                                 <h6><?= Yii::t('app', 'Owner & Breeder')?></h6>
-                                <h6><?= Yii::t('app', "Nursery «".Yii::$app->name."»")?></h6>
+                                <h6><?= Yii::t('app', "Nursery «Home of little joys»")?></h6>
                             </div>
                         </div>
                         <div class="col-md-6 nav-align-center">
