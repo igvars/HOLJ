c0797ce4d266cf7c6c13caf655e8cfcdfc4cc48e
sergey <sergeyusatyuk@gmail.com>
2016-08-19 14:27:45 +0300

    add yii

--- views/site/contacts.php
+++ views/site/contacts.php
@@ -9,60 +9,46 @@ use yii\bootstrap\ActiveForm;
 use yii\captcha\Captcha;
 
 $this->title = 'Contact';
-$this->params['breadcrumbs'][] = $this->title;
 ?>
-<div class="site-contact">
-    <h1><?= Html::encode($this->title) ?></h1>
+<div class="profile-page">
+    <div class="wrapper">
+        <div class="header header-filter" style="background-image: url('img/examples/city.jpg');"></div>
 
-    <?php if (Yii::$app->session->hasFlash('contactFormSubmitted')): ?>
-
-        <div class="alert alert-success">
-            Thank you for contacting us. We will respond to you as soon as possible.
-        </div>
-
-        <p>
-            Note that if you turn on the Yii debugger, you should be able
-            to view the mail message on the mail panel of the debugger.
-            <?php if (Yii::$app->mailer->useFileTransport): ?>
-                Because the application is in development mode, the email is not sent but saved as
-                a file under <code><?= Yii::getAlias(Yii::$app->mailer->fileTransportPath) ?></code>.
-                Please configure the <code>useFileTransport</code> property of the <code>mail</code>
-                application component to be false to enable email sending.
-            <?php endif; ?>
-        </p>
-
-    <?php else: ?>
-
-        <p>
-            If you have business inquiries or other questions, please fill out the following form to contact us.
-            Thank you.
-        </p>
-
-        <div class="row">
-            <div class="col-lg-5">
-
-                <?php $form = ActiveForm::begin(['id' => 'contact-form']); ?>
-
-                    <?= $form->field($model, 'name') ?>
-
-                    <?= $form->field($model, 'email') ?>
-
-                    <?= $form->field($model, 'subject') ?>
-
-                    <?= $form->field($model, 'body')->textArea(['rows' => 6]) ?>
-
-                    <?= $form->field($model, 'verifyCode')->widget(Captcha::className(), [
-                        'template' => '<div class="row"><div class="col-lg-3">{image}</div><div class="col-lg-6">{input}</div></div>',
-                    ]) ?>
-
-                    <div class="form-group">
-                        <?= Html::submitButton('Submit', ['class' => 'btn btn-primary', 'name' => 'contact-button']) ?>
+        <div class="main main-raised">
+            <div class="profile-content">
+                <div class="container">
+                    <div class="row">
+                        <div class="profile">
+                            <div class="avatar">
+                                <img src="https://pp.vk.me/c633619/v633619189/2d1bb/-VyCwY4_r_g.jpg" alt="Circle Image" class="img-circle img-responsive img-raised">
+                            </div>
+                            <div class="name">
+                                <h3 class="title main-padding-top">Леся Усатюк</h3>
+                                <h6>Владелец & Заводчик</h6>
+                                <h6>Питомник «Home of little joys»</h6>
+                            </div>
+                        </div>
                     </div>
-
-                <?php ActiveForm::end(); ?>
-
+                    <div class="contact-container">
+                        <div class="row">
+                            <div class="col-md-6 nav-align-center">
+                                <h3 class="text-primary"><i class="fa fa-map-marker"></i> Мы находимся</h3>
+                                <p>
+                                    Винница,<br>
+                                    Украина
+                                </p>
+                            </div>
+                            <div class="col-md-6 nav-align-center">
+                                <h3 class="text-primary"><i class="fa fa-phone"></i> Как связаться</h3>
+                                <p>
+                                    Леся Усатюк<br>
+                                    <a href="tel:+380636659660">+380 63 665 96 60</a><br>
+                                    <a href="tel:+380677005275">+380 67 700 52 75</a><br>
+                                    Пн - Пт, 10:00-20:00
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
-
-    <?php endif; ?>
-</div>
--- views/site/our-friends.php
+++ views/site/our-friends.php
@@ -4,15 +4,30 @@
 
 $this->title = Yii::$app->name;
 ?>
-<div class="site-index">
+<div class="profile-page">
+    <div class="wrapper">
+        <div class="header header-filter" style="background-image: url('img/examples/city.jpg');"></div>
 
-    <div class="body-content">
-
-        <div class="row">
-            <div class="col-xs-12">
-                <h2><?= Yii::t('app','Our friends')?></h2>
+        <div class="main main-raised">
+            <div class="profile-content">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-12">
+                            <h2>Официальные сайты кинологических федераций и клубов</h2>
+                            <a href="http://www.fci.be/" target="_blank"><img src="images/fci_logo1.png" alt="fci logo1"></a>
+                            <a href="http://www.uku.com.ua/" target="_blank"><img src="images/uku.png" alt="uku"></a>
+                            <a href="http://vinksu.ho.ua/" target="_blank"><img src="images/vinksu1.jpg" alt="vinksu1"></a>
+                            <h2>Наши друзья</h2>
+                            <ul class="our-friends-list">
+                                <li>
+                                    <a href="http://www.fci.be/" target="_blank">Питомник 1</a>
+                                </li>
+                                <li>
+                                    <a href="http://www.fci.be/" target="_blank">Питомник 2</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
-
-    </div>
-</div>
