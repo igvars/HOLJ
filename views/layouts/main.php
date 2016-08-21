5e1a1a501f5f26d27de6ebbf56b3022a43034961
romakvasha <romajan.ru@gmail.com>
2016-08-21 16:13:48 +0300

    change footer

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -90,19 +90,12 @@ AppAsset::register($this);
 
                         <footer class="footer">
                             <div class="container">
-                                <nav class="pull-left">
-                                    <ul>
-                                        <li>
-                                            <a href="<?= Yii::$app->homeUrl ?>">
-                                                <?= Yii::$app->name ?>
-                                            </a>
-                                        </li>
-                                    </ul>
-                                </nav>
                                 <div class="copyright pull-right">
-                                    &copy; <?= date('Y') > 2016 ? "2016-" . date('Y') : date('Y') ?><!--, made with <i class="fa fa-heart heart"></i> by Creative Tim-->
+                                    <a href="<?= Yii::$app->homeUrl ?>">
+                                        <?= Yii::$app->name ?>
+                                    </a>
+                                    &copy; <?= date('Y') > 2016 ? "2016-" . date('Y') : date('Y') ?>
                                 </div>
-
                             </div>
                         </footer>
                     </div>
